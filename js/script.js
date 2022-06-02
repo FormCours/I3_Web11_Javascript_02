53e0d15b9c0e79047c8b25c155835057b33585e1
Aurélien <aurelien.strimelle@bstorm.be>
2022-06-02 14:03:38 +0200

    Hello World ♥

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Récuperation du button !
+const btnTest = document.getElementById('btn-test');
+console.log(btnTest);
+
+// Ecoute de l'event click !
+btnTest.addEventListener('click', () => {
+    console.log('Hello World ♥');
+});
+
+
+
